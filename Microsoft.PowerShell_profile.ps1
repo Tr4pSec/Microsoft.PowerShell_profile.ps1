3bd906b4fc39e34b9f1b77443f1e8ff4f07e34d2
Max <maximilian.larum@gmail.com>
2019-01-31 15:05:08 +0100

    Update Microsoft.PowerShell_profile.ps1
    Updated profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,45 +1,68 @@
 set-location C:\
-$shell = $host.UI.RawUI
-$shell.WindowTitle = “Tr4pSec”
-$shell.BackgroundColor = “Black”
-$shell.ForegroundColor = “Cyan”
-function Get-Excuse {
-    (Invoke-WebRequest http://pages.cs.wisc.edu/~ballard/bofh/excuses -OutVariable excuses).content.split([Environment]::NewLine)[(get-random $excuses.content.split([Environment]::NewLine).count)]
+Import-Module PSConsoleTheme
+Set-ConsoleTheme 'Redmond'
+clear-host
+
+Function prompt {
+
+    if ($env:userdomain -AND $env:username) {
+        $me = "$($env:userdomain)\$($env:username)"
+    }
+    elseif ($env:LOGNAME) {
+        $me = $env:LOGNAME
+    }
+    else {
+        #last resort
+        $me = "PSUser"
+    }
+    
+
+    #define lines of text to include in the box
+    $text1 = "[$me] $($executionContext.SessionState.Path.CurrentLocation)"
+    $text2 = ((Get-Date).ToString()).trim()
+
+    if ($IsLinux) {
+        if ($(id -g) -eq 0 ) {
+            #running as SU
+            $lineColor = "Red"
+        }
+        else {
+            $lineColor = "Green"
+        }
+    }
+    elseif ($isWindows -or $psEdition -eq 'desktop') {
+        $IsAdmin = [System.Security.Principal.WindowsPrincipal]::new([System.Security.Principal.WindowsIdentity]::GetCurrent() ).IsInRole("Administrators")
+        if ($IsAdmin) {
+            $lineColor = "Red"
+        }
+        else {
+            $lineColor = "Green"
+        }
+    }
+    else {
+        #for everything else not tested
+        $lineColor = "Yellow"
+    }
+
+    #get the length of the longest line in the box and uas that to calculate lengths and padding
+    $longest = $text1.length, $text2.length | Sort-Object | Select-Object -last 1
+    $len = $longest + 2
+    $top = "┌$(("─" * $len))┐"
+    Write-Host $top -ForegroundColor $lineColor
+    Write-Host "│ " -ForegroundColor $lineColor -NoNewline
+    Write-Host $text1.PadRight($longest, ' ') -NoNewline
+    Write-Host " │" -ForegroundColor $lineColor
+    Write-Host "│ " -ForegroundColor $lineColor -NoNewline
+    Write-Host $text2.PadRight($longest, ' ') -NoNewline -ForegroundColor yellow
+    Write-Host " │" -ForegroundColor $lineColor
+    $bottom = "└$(("─" * $len))┘"
+    Write-Host $bottom -ForegroundColor $lineColor
+
+    #parse out the PSVersionTable to most meaningful values
+    $ver = $(([regex]"\d+\.\d+.\d+").match($psversiontable.psversion).value)
+
+    #the prompt function needs to write something to the pipeline
+    " v$($ver) PS$('>' * ($nestedPromptLevel + 1)) "
+
 }
-function get-dice {
-$dice = get-random -Maximum 6 -Minimum 1
-write-host $dice
-}
-Clear-host
-$block = @"
  
-.     .       .  .   . .   .   . .    +  .
-  .     .  :     .    .. :. .___---------___.
-       .  .   .    .  :.:. _".^ .^ ^.  '.. :"-_. .
-    .  :       .  .  .:../:            . .^  :.:\.
-        .   . :: +. :.:/: .   .    .        . . .:\
- .  :    .     . _ :::/:               .  ^ .  . .:\
-  .. . .   . - : :.:./.                        .  .:\
-  .      .     . :..|:                    .  .  ^. .:|
-    .       . : : ..||        .                . . !:|
-  .     . . . ::. ::\(                           . :)/
- .   .     : . : .:.|. ######              .#######::|
-  :.. .  :-  : .:  ::|.#######           ..########:|
- .  .  .  ..  .  .. :\ ########          :######## :/
-  .        .+ :: : -.:\ ########       . ########.:/
-    .  .+   . . . . :.:\. #######       #######..:/
-      :: . . . . ::.:..:.\           .   .   ..:/
-   .   .   .  .. :  -::::.\.       | |     . .:/
-      .  :  .  .  .-:.":.::.\             ..:/
- .      -.   . . . .: .:::.:.\.           .:/
-.   .   .  :      : ....::_:..:\   ___.  :/
-   .   .  .   .:. .. .  .: :.:.:\       :/
-     +   .   .   : . ::. :.:. .:.|\  .:/|
-     .         +   .  .  ...:: ..|  --.:|
-.      . . .   .  .  . ... :..:.."(  ..)"
- .   .       .      :  .   .: ::/  .  .::\
- 
-"@
- 
-Write-Host $block -ForegroundColor Green
-function Global:prompt {" $ "} 
